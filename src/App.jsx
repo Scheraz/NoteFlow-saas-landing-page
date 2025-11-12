7b79b34379cdcecc77dbd70172a43f7e85904c1f
Scheraz <sherazkhanpk.sk@gmail.com>
2025-11-12 17:45:36 +0800

    three sections developed Navbar, Logo section and features section

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+//Page
+import Page from "./components/sections/Page";
+
+//Header
+import Header from "./components/sections/Header";
+
+//Navigation
+import Navigation from "./components/sections/Navigation";
+import Hero from "./components/sections/Hero";
+import Reviews from "./components/sections/Reviews";
+import Main from "./components/sections/Main";
+import Logos from "./components/sections/Logos";
+import Features from "./components/sections/Features";
+
+function App() {
+  return (
+    <Page>
+      <Header>
+        <Navigation />
+        <Hero />
+        <Reviews />
+      </Header>
+      <Main>
+        <Logos />
+        <Features />
+      </Main>
+    </Page>
+  );
+}
+
+export default App;
---
+++ src/components/sections/Header.jsx
@@ -0,0 +1,4 @@
+const Header = ({ children }) => {
+  return <header className="bg-gradient-to-bottom py-10">{children}</header>;
+};
+export default Header;
